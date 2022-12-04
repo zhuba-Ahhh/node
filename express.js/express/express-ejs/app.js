87b5c87a9182ea32eab4c714ed2ab36f6a5378fe
zhuba-Ahhh <3477826311@qq.com>
2022-12-05 03:24:06 +0800

    fix: express生成器

--- express.js/express/express-ejs/app.js
+++ express.js/express/express-ejs/app.js
@@ -10,27 +10,29 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //
+app.set('view engine', 'ejs'); //
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter); //
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message; // 可以挂载到上下文 所以ejs中能被使用
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
--- express.js/express/express-ejs/routes/users.js
+++ express.js/express/express-ejs/routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  console.log(req.cookies);
+  res.send('zhuba');
 });
 
 module.exports = router;
